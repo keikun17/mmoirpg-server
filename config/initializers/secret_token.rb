d9c70fdd8c72eac8482244b6c5311538a8e2720d
Buddy Magsipoc <keikun17@gmail.com>
2013-10-11 12:37:11 +0800

    Initial commit。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MmoirpgServer::Application.config.secret_key_base = '488c2f4335b3b566f1a0b4c5b4fe62f016037cd2635f46b7a88af804e37a92855a9f505269ff42ed2b31699b2cd6ca027dba92ce92bbd5b401d36e5028833474'
