df7ac78e3235aa1a1d5185b91861a475838d0747
Buddy Magsipoc <keikun17@gmail.com>
2013-10-13 23:53:54 +0800

    Character form, index。

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,6 +3,22 @@ class CharactersController < ApplicationController
   before_filter :authenticate_player!
 
   def index
+    @characters = Character.all
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    Character.create(character_params)
+    redirect_to :index
+  end
+
+  private
+
+  def character_params
+    params[:character].permit([:name, :strength, :vitality])
   end
 
 end
