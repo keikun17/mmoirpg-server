6e6238a55ae13e5ea3e6e6c35ab36c12120d27be
Buddy Magsipoc <keikun17@gmail.com>
2013-10-15 10:36:56 +0800

    Sidebar。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ MmoirpgServer::Application.routes.draw do
   devise_for :players
 
   resources :characters
+  resources :battles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
