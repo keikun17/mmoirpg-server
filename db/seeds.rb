5a6ef444681a2aaf2a8370a9c299f782e9bf5ca2
Buddy Magsipoc <keikun17@gmail.com>
2013-10-15 12:38:04 +0800

    Seed player。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Player.create(email: 'user@email.com', password: 'password', password_confirmation: 'password')
