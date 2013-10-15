8eac063d1de0c2816812ffbefb0dde972da68bbb
Buddy Magsipoc <keikun17@gmail.com>
2013-10-15 10:37:36 +0800

    Battle resource。

---
+++ app/controllers/battle_controller.rb
@@ -0,0 +1,6 @@
+class BattleController < ApplicationController
+
+  def index
+  end
+
+end
---
+++ app/models/battle.rb
@@ -0,0 +1,17 @@
+class Battle < ActiveRecord
+  has_many :characters
+
+  def self.start
+    battle = Battle.create
+
+    characters = Character.all
+    characters.shuffle
+
+    character_1 = characters.shift
+    character_2 = characters.shift
+
+    PlayerBattle.create(
+      battle: battle, character_1: character_1, character_2: character_2
+    )
+  end
+end
